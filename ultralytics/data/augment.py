484e4773dc17dedb2ad984cc5b01712d3739fc3c
BenjaminLi <76795078+25benjaminli@users.noreply.github.com>
2024-03-07 11:01:57 -0500

    Update augment.py

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -837,13 +837,14 @@ class Albumentations:
 
             # Transforms
             T = [
-                A.Blur(p=0.01),
-                A.MedianBlur(p=0.01),
-                A.ToGray(p=0.01),
-                A.CLAHE(p=0.01),
-                A.RandomBrightnessContrast(p=0.0),
-                A.RandomGamma(p=0.0),
-                A.ImageCompression(quality_lower=75, p=0.0),
+                # A.Blur(p=0.01),
+                # A.MedianBlur(p=0.01),
+                # A.ToGray(p=0.01),
+                # A.CLAHE(p=0.01),
+                A.RandomBrightnessContrast(p=0.2, contrast_limit=0), # brightness_limit=0.2, contrast_limit=0.2, brightness_by_max=True, always_apply=False, p=0.5
+                # A.RandomGamma(p=0.0),
+                # A.ImageCompression(quality_lower=75, p=0.0),
+                
             ]
             self.transform = A.Compose(T, bbox_params=A.BboxParams(format="yolo", label_fields=["class_labels"]))
 
